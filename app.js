767229e5749be97c83c5a9ab34773c4d6631e277
Zach Garrett <zacharytheodoregarrett@gmail.com>
2021-08-22 16:09:10 -0700

    styling fetch classifications populate dropdown

--- app.js
+++ app.js
@@ -1,19 +1,70 @@
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2'
 const API_KEY = 'apikey=XtC3y6Uo8LjeL5IgPrEWm8kRqzY7kR3p';
+const classificationsArray = [];
+async function fetchEvents() {
+    const url = `${BASE_URL}/events.json?${API_KEY}`;
 
-function fetchEvents() {
-    const url = `${BASE_URL}/events.json?countryCode=US&${API_KEY}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
 
-    fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function(response) {
-            console.log(response);
-        })
-        .catch(function(error) {
-            console.error(error);
-        });
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 fetchEvents();
+
+async function fetchAllClassifications() {
+    const url = `${BASE_URL}/classifications.json?${API_KEY}`;
+    const local = localStorage.getItem('data');
+
+    if (local) {
+        const retrievedObject = JSON.parse(local);
+        const localType = retrievedObject._embedded.classifications;
+        const localCName = localType.forEach((one) => {
+            if(one.segment){
+                const actualClassifications = one.segment.name;
+                classificationsArray.push(actualClassifications);
+                console.log(actualClassifications);
+                return actualClassifications;
+            }
+        })
+        return;
+    };
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        localStorage.setItem('data', JSON.stringify(data));
+        const type = data._embedded.classifications;
+        const cName = type.forEach((one) => {
+            if(one.segment){
+                const actualClassifications = one.segment.name;
+                classificationsArray.push(actualClassifications);
+                return actualClassifications;
+            }
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function prefetchList() {
+    try {
+    const classifications = await fetchAllClassifications();
+    console.log('array', classificationsArray);
+    $('.classification-count').text(`(${ classificationsArray.length })`);
+
+    classificationsArray.forEach(classification => {
+        const optionElement = $(`<option value='${classification}'>${classification}</option>`)
+        $('#select-classification').append(optionElement)
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  prefetchList();
